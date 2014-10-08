d06f78a65116a171dd58d0650241c2273c932e7d
KavilanNaidoo <40904@longroad.ac.uk>
2014-10-08 20:41:48 +0100

    ascii exercise

---
+++ ascii exercise.py
@@ -0,0 +1,12 @@
+#Kavilan Naidoo
+#06-10-2014
+#Ascii exercise
+
+
+number=int(input("Please enter an ascii number: "))
+character= chr(number)
+print("Your ascii number as a character is {0}".format(character))
+
+text=input("Please enter a character: ")
+character= ord(text)
+print("Your character as an ascii number is {0}".format(character))
